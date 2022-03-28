2f6414132d0c4bdf41a34eb46f666ae02e731e42
Luke78966 <99269373+Luke78966@users.noreply.github.com>
2022-03-28 16:01:37 -0500

    Create strings.js

---
+++ chatbot/scripts/strings.js
@@ -0,0 +1,67 @@
+/* 
+strings.js
+
+This file contains all of the precoded strings that the chatbot will use
+to respond to the user. This file provides an easy way to edit the messages
+used by the chatbot.
+*/
+
+
+/*****************************
+ ***  PLEASANTRY RESPONSES
+ *****************************/
+const WELCOME_MESSAGE = "Welcome to Newzeen!";
+const HELLO_RESPONSE = "Hello!";
+const GOODBYE_RESPONSE = "Goodbye!";
+
+
+/*******************************
+ ***   PLEASANTRY ARRAYS
+ *******************************/
+const PLEASANTRY_KEYWORDS = ["hello", "hey", "hi", "bye", "goodbye"];
+
+const GREETING_KEYWORDS = ["hello", "hey", "hi"];
+
+const FAREWELL_KEYWORDS = ["bye", "goodbye"];
+
+
+/*********************************
+ ***    REVIEW RESPONSES
+ *********************************/
+
+const REVIEW_RESPONSE = "Ok! I'll bring you to our review page!";
+
+/********************************
+ ***    REVIEW ARRAYS 
+ ********************************/
+const REVIEW_KEYWORDS = ["review", "feedback", "rating", "evaluation", "assessment", "critique"];
+
+
+/*******************************
+ ***    ACCOUNT RESPONSES 
+ *******************************/
+
+const ACCOUNT_RESPONSE = "Ok! I'll bring you to your account page!";
+
+/*******************************
+ ***    ACCOUNT ARRAYS
+ *******************************/ 
+
+const ACCOUNT_KEYWORDS = ["account", "profile"]; 
+
+
+/******************************
+ ***    RETURN RESPONSES
+ ******************************/
+const RETURN_RESPONSE = "Ok! I'll bring you to your purchased items!";
+
+/******************************
+ ***    RETURN ARRAYS 
+ ******************************/
+const RETURN_KEYWORDS = ["return"];
+
+
+/*****************************
+ ***  DEFAULT MESSAGES
+ *****************************/
+const DEFAULT_MESSAGE = "I don't know how to respond to that</br></br>Try asking something like 'I'd like to write a review' or 'Return an item'";
